e5c69997d4e17a72accc7e8fb5d3c1564f97aa15
Jing Jie <jingjie.24@ichat.sp.edu.sg>
2025-05-11 17:30:06 +0800

    lab 3 code

--- test_Lab3.py
+++ test_Lab3.py
@@ -28,3 +28,27 @@ def test_bubble_sort_invalid():
     result = Lab3.bubble_sort(input_arr, 3)
 
     assert (result == [])
+
+def test_bubble_sort_many():
+    result = 0
+    input_arr = [64, 34, 25, 12, 22, 11, 90, 88, 77, 66]
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+
+    assert (result == 1)
+
+def test_bubble_sort_zero():
+    result = 1
+    input_arr = []
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+
+    assert (result == 0)
+
+def test_bubble_sort_not():
+    result = 0
+    input_arr = ["abc","dea","egewg","tfew"]
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+
+    assert (result == 2)
